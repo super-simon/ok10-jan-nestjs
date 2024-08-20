a012fd4744a05ae6094c49048838f0802c6d6a98
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2024-08-20 17:32:07 +0300

    lesson04/docker

--- src/config/config.type.ts
+++ src/config/config.type.ts
@@ -1,8 +1,30 @@
 export type Config = {
   app: AppConfig;
+  postgres: PostgresConfig;
+  redis: RedisConfig;
+  minio: MinioConfig;
 };
 
 export type AppConfig = {
   port: number;
   host: string;
+};
+
+export type PostgresConfig = {
+  port: number;
+  host: string;
+  user: string;
+  password: string;
+  db: string;
+};
+
+export type RedisConfig = {
+  port: number;
+  host: string;
+  password: string;
+};
+
+export type MinioConfig = {
+  user: string;
+  password: string;
 };
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,4 +3,20 @@ export default () => ({
     port: Number(process.env.APP_PORT) || 3000,
     host: process.env.APP_HOST || 'localhost',
   },
+  postgres: {
+    port: Number(process.env.POSTGRES_PORT) || 5432,
+    host: process.env.POSTGRES_HOST || 'localhost',
+    user: process.env.POSTGRES_USER || 'user',
+    password: process.env.POSTGRES_PASSWORD || 'password',
+    db: process.env.POSTGRES_DB || 'jan-2024',
+  },
+  redis: {
+    port: Number(process.env.REDIS_PORT) || 6379,
+    host: process.env.REDIS_HOST || 'localhost',
+    password: process.env.REDIS_PASSWORD || 'password',
+  },
+  minio: {
+    user: process.env.MINIO_ROOT_USER || 'user',
+    password: process.env.MINIO_ROOT_PASSWORD || 'password',
+  },
 });
