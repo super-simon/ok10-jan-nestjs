1719af1e40485f4c2b383b1b88167c0286079faa
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2024-09-05 18:50:51 +0300

    Lesson07/relations

---
+++ src/database/entities/article.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { LileEntity } from './like.entity';
+import { CreateUpdateModel } from './models/create-update.model';
+import { TagEntity } from './tag.entity';
+import { UserEntity } from './user.entity';
+
+@Entity('articles')
+export class ArticleEntity extends CreateUpdateModel {
+  @Column('text')
+  title: string;
+
+  @Column('text')
+  description: string;
+
+  @Column('text')
+  body: string;
+
+  @OneToMany(() => LileEntity, (entity) => entity.article)
+  likes?: LileEntity[];
+
+  @Column()
+  user_id: string;
+  @ManyToOne(() => UserEntity, (entity) => entity.articles)
+  @JoinColumn({ name: 'user_id' })
+  user?: UserEntity;
+
+  @ManyToMany(() => TagEntity, (entity) => entity.articles)
+  @JoinTable()
+  tags?: TagEntity[];
+}
---
+++ src/database/entities/like.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { ArticleEntity } from './article.entity';
+import { CreateUpdateModel } from './models/create-update.model';
+import { UserEntity } from './user.entity';
+
+@Entity('likes')
+export class LileEntity extends CreateUpdateModel {
+  @Column()
+  user_id: string;
+  @ManyToOne(() => UserEntity, (entity) => entity.likes)
+  @JoinColumn({ name: 'user_id' })
+  user?: UserEntity;
+
+  @Column()
+  article_id: string;
+  @ManyToOne(() => ArticleEntity, (entity) => entity.likes)
+  @JoinColumn({ name: 'article_id' })
+  article?: ArticleEntity;
+}
