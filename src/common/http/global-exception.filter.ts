4b5acf70c7db77cf0326d58c32720250453b2128
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2024-09-10 22:05:28 +0300

    error filters

--- src/common/http/global-exception.filter.ts
+++ src/common/http/global-exception.filter.ts
@@ -6,7 +6,10 @@ import {
   HttpException,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { LoggerService } from 'src/modules/logger/logger.service';
+import { QueryFailedError } from 'typeorm';
+
+import { LoggerService } from '../../modules/logger/logger.service';
+import { DbQueryFailedFilter } from './db-query-failed.filter';
 
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
@@ -16,23 +19,27 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
+
     let status: number;
     let messages: string[] | string;
+
     if (exception instanceof BadRequestException) {
       status = exception.getStatus();
       messages = (exception.getResponse() as any).message;
     } else if (exception instanceof HttpException) {
       status = exception.getStatus();
       messages = exception.message;
+    } else if (exception instanceof QueryFailedError) {
+      const error = DbQueryFailedFilter.filter(exception);
+      status = error.status;
+      messages = error.message;
     } else {
       status = 500;
-      messages = 'Internal Server Error';
+      messages = 'Internal server error';
     }
-
     this.logger.error(exception);
 
     messages = Array.isArray(messages) ? messages : [messages];
-
     response.status(status).json({
       statusCode: status,
       messages,
